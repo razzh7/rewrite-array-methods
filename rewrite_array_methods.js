ec73a5e9c2ddc585e86dcdea7636d0d3fa2257c3
Razzh <136013779@qq.com>
2021-07-26 22:57:08 +0800

    重写数组方法

--- rewrite_array_methods.js
+++ rewrite_array_methods.js
@@ -9,12 +9,27 @@
  * every
  * some
  */
+ /**
+  * 不会遍历稀疏数组空隙的方法
+  * forEach
+  * some
+  * every
+  * map
+  * reduce
+  * filter
+  * 
+  * 会遍历稀疏数组空隙的方法
+  * for
+  * find
+  * findIndex 
+  */
 /**
  * @method forEach
- * @description 对数组的每一个元素执行一次给定的方法，不会改变原数组的引用值，那些已删除或者未初始化的项将被跳过（例如在稀疏数组上）。
+ * @description 对数组的每一个元素执行一次给定的方法
  * @param {Function} callback
  * @param {any} arguments[1] 改变this指向
  * @this callback this默认指向window 
+ * @notice 不会改变原数组的引用值，那些已删除或者未初始化的项将被跳过（例如在稀疏数组上）
  */
 
 Array.prototype._forEach = function(callback){
@@ -23,6 +38,9 @@ Array.prototype._forEach = function(callback){
         thisArg = arguments[1] || window;
 
     for(var i = 0; i < len; i++){
+        if(arr[i] === undefined){
+          continue;
+        }
         callback.apply(thisArg, [arr[i], i, arr]);
     }
 }
@@ -220,13 +238,304 @@ function generate(arr){
  * @returns {Array Iterator} 一个新的Array 迭代器对象。Array Iterator是对象，由Object函数构造
  *                           它的原型（__proto__:Array Iterator）上有一个next方法，
  *                           可用于遍历迭代器取得原数组的[index, value]  
- **/
+ */
 
  Array.prototype._entries = function *(){
     for(var i = 0; i < this.length; i++){
       yield [i, this[i]];  //生成器对迭代器的控制是通过yield关键字执行的
     }    
 }
+
+/**
+ * @method fill
+ * @descirption 改变原数组，替换指定位置的数组值
+ * @param
+ *   value
+ *      要替换的值
+ *   start
+ *      从哪里开始替换 闭区间
+ *   end
+ *      到哪里替换结束 开区间
+ *@notice
+ * start取值
+ * 1、start < 0 start从start + len开始取数替换
+ * 2、start > 0 从 start 开始取数替换
+ * 3、start必须小于end，否则返回原数组
+ * 4、start为假时，start取0 
+ * end取值
+ * 1、end为假时，end取len
+ * 2、end为null时返回原数组
+ * 3、end为undefined时 end取len
+ * 4、end < 0 时 end取 len + end
+ * 5、end > length时 end取len
+ * */
+
+ Array.prototype._fill = function(value, start, end){
+    if(this === void 0 || this === null) {
+        throw new TypeError();
+    }
+
+    var len = this.length;
+    if(start === undefined) {
+        start = 0;
+    }
+    if(end === undefined) {
+        end = len;
+    }
+
+    start = Number(start);
+    start = isNaN(start) ? 0 : start;
+    end = Number(end);
+    end = isNaN(end) ? 0 : end;
+    start += start >= 0 ? 0 : length;
+    end += end >= 0 ? 0 : length;
+
+    while(start < end){
+        this[start] = value;
+        start++;
+    }
+
+    return this;
+ }
+
+ /**
+  * @method find ES6 
+  * @description find() 方法返回数组中满足提供的测试函数的第一个元素的值。否则返回 undefined。
+  * @param
+  *   callback
+  *     在数组每一项上执行的函数，接收 3 个参数
+  *     element
+  *       当前数组遍历到的元素
+  *     index
+  *       当前遍历数组元素的索引值
+  *     arrary
+  *       数组本身
+  * @notice 
+  *  1、返回的元素和数组对于下标的元素是同一个引用
+  *  2、find会遍历稀松数组的空隙empty，具体遍历出的值，由undefined占位
+  *  3、find的遍历效率是低于ES5数组扩展方法的
+  *  4、对于splice删除了对应项，该项位置不保留，在数组最后补上undefined
+  *  5、使用delete、pop 删除该项的值并填入undefined
+  *  6、find的函数内部指向：
+  *         在非严格模式环境下，this -> window
+  *         严格模式下，不传入第二个参数，this为undefined,与严格模式规定相统一
+  *  7、回调函数内部无法改变原数组的元素值
+  *  8、如果使用push方法向原数组加入数据，原数组可以被push进数据
+  *     但find在第一次调用回调函数的时候就确认了数组的长度array.length，只会遍历原数组的长度
+  *  9、回调函数返回一个bool值
+  */
+
+  Array.prototype._find = function(callback){
+    if(this == void 0 || this == null){
+        throw new TypeError('"this" is null');
+    }
+    if(typeof callback !== 'function'){
+        throw new TypeError('callback must be function');
+    }
+    var obj = Object(this),  //包装this确保this一直是引用值不变
+        len = obj.length >>> 0, //保证len是正整数
+        thisArg = arguments[1];  //第二项没有参数，那么传入undefined，严格模式下this为undefined
+
+        for(var i = 0; i < len; i++){
+            if(callback.apply(thisArg, [obj[i], i, obj])){
+                return obj[i];
+            }
+        }
+
+        return undefined;
+  }
+/**
+ * @method findIndex ES6
+ * @descirption findIndex方法返回数组中满足提供的测试函数的第一个元素的索引，若没有找到对应元素则返回-1
+ * @param
+ *   callback
+ *     在数组每一项上执行的函数，接收 3 个参数
+ *     element
+ *       当前数组遍历到的元素
+ *     index
+ *       当前遍历数组元素的索引值
+ *     arrary
+ *       数组本身
+ * @notice 参考find
+ * */
+  Array.prototype._findIndex = function(callback){
+    if(this == void 0 || this == null){
+        throw new TypeError('"this" is null');
+    }
+    if(typeof callback !== 'function'){
+        throw new TypeError('callback must be function');
+    }
+    var obj = Object(this),
+        len = obj.length >>> 0,
+        thisArg = arguments[1];
+
+        for(var i = 0; i < len; i++){
+            if(callback.apply(thisArg, [obj[i], i, obj])){
+                return i;
+            }
+        }
+
+        return -1;
+  }
+
+  /**
+   * @method values ES6
+   * @description values方法返回一个新的Array Iterator对象，该对象包含数组每个索引的值
+   * @notcie 
+   *  1、可以通过for of来遍历迭代值
+   *  2、会遍历稀疏数组的空隙值，打印出undefined
+   */
+
+ Array.prototype._values = function *(){
+    if(this === null){
+        throw new TypeError('"this" is null');
+    }
+    var arr = this,
+        len = arr.length >>> 0;
+
+        for(var i = 0; i < len; i++){
+            yield arr[i];
+        }
+ }
+
+ /**
+  * @method keys ES6 
+  * @description keys方法会返回一个新的Array Iterator对象，该对象包含数组元素的索引
+  * @notice
+  * 1、可以通过for of来遍历迭代值
+  * 2、会遍历稀疏数组的空隙值，打印出undefined
+  */
+
+  Array.prototype._keys = function *(){
+    if(this === null){
+        throw new TypeError('"this" is null');
+    }
+    var arr = this,
+        len = arr.length >>> 0,
+        i = 0;
+
+        while(i < len){
+            yield i;
+            i++;
+        }
+  }
+
+/**
+ * @method push
+ * @description puhs方法会往原数组最后一项加入元素，返回加入元素后数组的长度
+ * @notice 会改变原数组
+ */
+  Array.prototype._push = function(){
+    var len = arguments.length;
+
+    for(var i = 0; i < len; i++){
+        this[this.length] = arguments[i];
+    }
+    return this.length;
+
+  }
+
+/**
+ * @method unshift 
+ * @description unshift方法会将一个或多个元素添加到数组的开头，并返回该数组新的长度
+ * @notice 
+ *  1、该方法会改变原数组
+ *  2、多个参数调用一次unshift和传入一个参数调用多次unshift，会得到不同的结果 -> MDN
+ */
+
+ Array.prototype._unshift = function(){
+    var len = arguments.length;
+
+        for(var i = 0; i < len; i++){
+            this.splice(0, 0, arguments[i]);
+        }
+
+        return this.length;
+ }
+
+ /**
+  * @method shift 
+  * @description shift方法从数组中删除第一个元素，并返回该元素的值
+  * @notice 
+  * 1、此方法更改数组长度
+  * 2、如果第一项为空，不会返回数组长度而会返回undefined，但第一项的空值还是能删除
+  */
+
+  Array.prototype._shift = function(){
+    if(this === null){
+        throw new TypeError('this is null');
+    }
+    if(this[0] === undefined){
+        this.splice(0, 1);
+        return undefined;
+    }
+
+    this.splice(0, 1);
+    return this.length;
+  }
+
+ /**
+  * @method pop 
+  * @description pop方法从数组中删除最后一个元素，并返回该元素的值
+  * @notice
+  *  1、此方法更改数组长度
+  *  2、如果length为0，返回undefined
+  *  3、稀疏数组最后一位为空，那么删除倒数第二位，并返回其值
+  */
+
+  Array.prototype._pop = function(){
+    var arr = this,
+        len = arr.length;
+    if(this === null){
+        throw new TypeError('this is null');
+    }
+    if(len <= 0){
+        return undefined;
+    }
+
+    var newArr = arr.splice(len - 1, 1);
+    return newArr[0];
+  }
+
+/**
+ * @method concat 
+ * @description concat方法用于合并两个或者多个数组，并返回一个新数组
+ * @notice 
+ * 此方法不会改变原数组
+ */
+
+
+/**
+ * @method toString  ES6
+ * @descriotion 将数组转换为字符串并返回
+ * @notice
+ * 1、此方法不会改变原数组
+ * 2、稀疏数组的空项可以被返回出来
+ * 3、用到了join方法，等待重写
+ */
+
+  Array.prototype._toString = function(){
+    var arr = this,
+        len = arr.length,
+        str = '';
+
+    if(this === null){
+        throw new TypeError('this is null');
+    }
+
+    return str += len > 0 ? arr.join(',') : '';
+  }
+
+ /**
+  * @method isArray  Array.isArray() 用于确定传递的值是否是一个 Array。
+  * @param obj 需要检查的值
+  * @return 如果值是Array，则为true，否则为false
+  */
+
+  Array._isArray = function(){
+
+  }
+
 
 
   
